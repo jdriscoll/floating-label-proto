3f8cb19d14b01126054550fc5d2bd48a5003e692
Justin Driscoll <justin@driscolldev.com>
2016-02-11 18:12:14 -0500

    WIP: Integrate float form field class

--- FloatLabel/ViewController.swift
+++ FloatLabel/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    self.tableView.rowHeight = UITableViewAutomaticDimension
+    self.tableView.estimatedRowHeight = 100
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,6 +23,17 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+
+  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    print("Height")
+    return UITableViewAutomaticDimension
+  }
+
+
+//
+//  override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+//    cell.preservesSuperviewLayoutMargins = true
+//  }
 
 
 }
